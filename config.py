7dcfa7359783a9af58ba867fa061cd687809314a
MrTimeGo <mrtimego@ukr.net>
2020-08-14 22:45:31 +0300

    bugs fixed

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'NzQzODY0NzcxMDI3NDAyOTIy.Xza4DQ.QcJK9xPonZ2bTBIAaM7BX4uuDvk-',
+    'token': 'NzQzODY0NzcxMDI3NDAyOTIy.Xza4DQ.QcJK9xPonZ2bTBIAaM7BX4uuDv',
     'bot': 'ValeraBot',
     'id': 743864771027402922,
     'prefix': '&'
